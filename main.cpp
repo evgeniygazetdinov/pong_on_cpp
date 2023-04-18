0dd117d69d33521bc8e4d7e49ba22228ea8eaa2c
Gazetdinov <yngazetd@mts.ru>
2023-04-18 04:11:41 +0300

    addition for make and working on person moving

--- main.cpp
+++ main.cpp
@@ -1,22 +1,49 @@
 #include <SFML/Graphics.hpp>
 
+using namespace sf;
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    RenderWindow window(VideoMode(640, 480), "SFML works!");
+
+    Image image;
+    image.loadFromFile("images/player.png");
+    Texture texture;
+    texture.loadFromImage(image);
+    // квадрат на который натягивается текстура
+    Sprite player;
+    player.setTexture(texture);     // ширина и высота
+    player.setTextureRect(IntRect(0, 0, 31, 32));
+    player.setPosition(10, 10);
+
 
     while (window.isOpen())
     {
-        sf::Event event;
+        Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed)
                 window.close();
+
+            
+        }
+
+        //move into another class
+        if(Keyboard::isKeyPressed(Keyboard::Left)){
+            player.move(-1.0, 0);
+        }
+        if(Keyboard::isKeyPressed(Keyboard::Right)){
+            player.move(1.0, 0);
+        }
+        if(Keyboard::isKeyPressed(Keyboard::Up)){
+            player.move(0, -1.0);
+        }
+        if(Keyboard::isKeyPressed(Keyboard::Down)){
+            player.move(0, 1.0);
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(player);
         window.display();
     }
 
--- make.sh
+++ make.sh
@@ -1,2 +1,11 @@
-eval "g++ main.cpp -I/usr/local/Cellar/sfml/2.5.1.2/includ/e -o game -L /usr/local/Cellar/sfml/2.5.1_2/lib -lsfml-graphics -lsfml-window -lsfml-system";                   
-eval "./game";
+
+FILE=game;
+COMPILE_TEXT="g++ main.cpp -I/usr/local/Cellar/sfml/2.5.1.2/includ/e -o game -L /usr/local/Cellar/sfml/2.5.1_2/lib -lsfml-graphics -lsfml-window -lsfml-system";
+if [ -f "$FILE" ]; then
+    eval "rm -rf $FILE";
+    eval $COMPILE_TEXT;                 
+    eval "./$FILE";
+else 
+    eval $COMPILE_TEXT;
+    eval "./$FILE";
+fi
