2b810644e4ad700fc8b9be3a094c7fcd38d2b5ca
Gazetdinov <yngazetd@mts.ru>
2023-04-26 06:24:49 +0300

    fixes for left and right moving

--- main_mario.cpp
+++ main_mario.cpp
@@ -3,87 +3,28 @@
 
 using namespace sf;
 
-float frame = 0.0;
-int ground = 150;
-
-class Player{
-  public:
-    FloatRect rect;
-    bool onGround; 
-
-    float x, y, weight, height, vx, vy, speed = 0;
-    int direction = 0;
-    String filename;
-    Image image;
-    Texture texture;
-    Sprite sprite;
-    float wayDistance = 0.5;
-    float animationSpeed = 0.08;
-    Player(int posX, int posY, int playerWeight, int playerHeight, String cartonName){
-        x = posX;
-        y = posY;
-        weight = playerWeight;
-        height = playerHeight;
-        filename = cartonName;
-        image.loadFromFile("images/" + filename);
-        texture.loadFromImage(image);
-        rect = FloatRect(0,0, 31,32);
-
-        // square for stretch texture
-        sprite.setTexture(texture); // ширина и высота
-        sprite.setTextureRect(IntRect(0, 0, weight, height));
-        sprite.setPosition(x, y);
-    }
-    void update(float time){
-      switch (direction)
-      {
-      case 0: vx = -speed; vy = 0; break;
-      case 1: vx = speed; vy = 0; break;
-      case 2: vx = 0; vy = -speed; break;
-      case 3: vx = 0; vy = speed; break;
-      x += vx * time;
-      y += vy * time;
-      speed = 0;
-      sprite.setPosition(x, y);
-      if(!onGround){
-        vy = vy + 0.005 *time;
-        rect.top += vy * time;
-        onGround = false;
-      }
-      if(rect.top >ground){
-        rect.top = ground;
-        vy = 0 ;
-        onGround = true;
-        sprite.setPosition(rect.left, rect.top);
-
-      }
-
-      }
-
-
-    }
-    
-
-};
+// 
 
 int main() {
-  RenderWindow window(VideoMode(640, 480), "my turbo game");
-  Player hero(50, 50, 31, 32, "player.png");
-  
 
-
-  //bound by time
+  float currentFrame = 0;
+  RenderWindow window(VideoMode(200, 200), "my turbo game");
+  Texture myTexture;
+  myTexture.loadFromFile("images/fang.png");
+  Sprite mySprite;
+  mySprite.setTexture(myTexture);
+                                 // x y weight height
+  mySprite.setTextureRect(IntRect(0, 244, 40, 50));
+  mySprite.setPosition(50, 100);
 
   Clock myclock;
 
   while (window.isOpen()) {
-    // recalculate time for more slow moving char
-    float time = myclock.getElapsedTime().asMicroseconds();
-    myclock.restart();
-    time = time / 1500;
+    // float time = myclock.getElapsedTime().asMicroseconds();
+    // myclock.restart();
+    // time = time / 1500;
     Event event;
 
-    // move variable
 
     while (window.pollEvent(event)) {
       if (event.type == Event::Closed)
@@ -91,44 +32,29 @@ int main() {
       if(Keyboard::isKeyPressed(Keyboard::Escape)){
         window.close();
       }
+      if (Keyboard::isKeyPressed(Keyboard::Left)) {
+          mySprite.move(-10, 0);
+          std::cout<<"LEFT"<<std::endl;
+           currentFrame += 0.005;
+         currentFrame += 1;
+          if(currentFrame >6){
+            currentFrame -=6;
+          }
+          // INVERTING POSITION 
+          mySprite.setTextureRect(IntRect(40 * int(currentFrame)+40, 244,-40,50));
+
+      }
+      if (Keyboard::isKeyPressed(Keyboard::Right)) {
+          mySprite.move(10, 0);
+          currentFrame += 1;
+          if(currentFrame >6){
+            currentFrame -=6;
+          }
+          mySprite.setTextureRect(IntRect(40 * int(currentFrame), 244,40,50));
+      }
     }
-    //move into another class
-    if (Keyboard::isKeyPressed(Keyboard::Left)) {
-      hero.direction = 0; hero.speed = hero.animationSpeed;
-      frame += 0.02;
-      hero.sprite.move(-1.0, 0);
-      if (frame > 2) frame -= 2;
-        hero.sprite.setTextureRect(IntRect(31 *int(frame),96, 31, 32));
-      }
-    if (Keyboard::isKeyPressed(Keyboard::Right)) {
-     hero.direction = 0; hero.speed = hero.animationSpeed;
-     frame += 0.02;
-     hero.sprite.move(1.0, 0);
-      if (frame > 2) frame -= 2;
-        hero.sprite.setTextureRect(IntRect(31 *int(frame),32, 31, 32));
-      }
-
-    if (Keyboard::isKeyPressed(Keyboard::Up)) {
-      hero.direction = 0; hero.speed = hero.animationSpeed;
-      frame += 0.02; 
-      if (frame > 2) frame -= 2;
-        hero.sprite.setTextureRect(IntRect(31 *int(frame),0, 31, 32));
-        if(hero.onGround){
-          hero.vy = -0.4; hero.onGround = false;
-        }
-
-
-      }
-    if (Keyboard::isKeyPressed(Keyboard::Down)) {
-     hero.direction = 0; hero.speed = hero.animationSpeed;
-     frame += 0.02;
-      if (frame > 2) frame -= 2;
-        hero.sprite.setTextureRect(IntRect(31 *int(frame),64, 31, 32));
-      }
-
-    hero.update(time);
     window.clear();
-    window.draw(hero.sprite);
+    window.draw(mySprite);
     window.display();
   }
 
--- make.sh
+++ make.sh
@@ -1,6 +1,7 @@
 
 FILE=game;
-COMPILE_TEXT="g++ main.cpp -I/usr/local/Cellar/sfml/2.5.1.2/includ/e -o game -L /usr/local/Cellar/sfml/2.5.1_2/lib -lsfml-graphics -lsfml-window -lsfml-system";
+COMPILED_FILE=main_mario.cpp
+COMPILE_TEXT="g++ "$COMPILED_FILE" -I/usr/local/Cellar/sfml/2.5.1.2/includ/e -o game -L /usr/local/Cellar/sfml/2.5.1_2/lib -lsfml-graphics -lsfml-window -lsfml-system";
 if [ -f "$FILE" ]; then
     eval "rm -rf $FILE";
     eval $COMPILE_TEXT;                 
