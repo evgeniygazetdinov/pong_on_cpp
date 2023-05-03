da3ea47730068d2a82df0da810a8fe0e2f32ed73
Gazetdinov <yngazetd@mts.ru>
2023-05-03 04:49:19 +0300

    fix movig but have issues with animation

--- main_mario.cpp
+++ main_mario.cpp
@@ -3,7 +3,55 @@
 
 using namespace sf;
 
-// 
+int ground = 150;
+
+class Player{
+  public:
+    float dx, dy;// speed variables
+    FloatRect rect;
+    bool onGround;
+    Sprite sprite;
+    float currentFrame;
+   Player(Texture &image){
+    sprite.setTexture(image);
+    rect = FloatRect(0, 0, 40, 50);
+    dx = dy = 0;
+    currentFrame = 0;
+   }
+   void update(float time){
+      rect.left += dx * time;
+      if(!onGround) {
+        dy=dy + 0.0005 * time;
+      } 
+      rect.top += dy * time;
+      onGround = false;
+      currentFrame += 0.005 * time;
+
+      if(currentFrame >6){
+        currentFrame -=6;
+      }
+      sprite.setTextureRect(IntRect(40 * int(currentFrame), 244,40,50));
+      if(dx>0){
+        sprite.setTextureRect(IntRect(40 * int(currentFrame), 244,40,50));
+      }
+      if(dx<0){
+        sprite.setTextureRect(IntRect(40 * int(currentFrame)+40, 244,-40,50));
+      }
+
+      
+      if(rect.top > ground){
+        rect.top = ground;
+        dy = 0;
+        onGround = true;
+      } 
+      sprite.setPosition(rect.left, rect.top);
+      dx = 0;
+   }
+
+};
+
+
+
 
 int main() {
 
@@ -11,12 +59,9 @@ int main() {
   RenderWindow window(VideoMode(200, 200), "my turbo game");
   Texture myTexture;
   myTexture.loadFromFile("images/fang.png");
-  Sprite mySprite;
-  mySprite.setTexture(myTexture);
-                                 // x y weight height
-  mySprite.setTextureRect(IntRect(0, 244, 40, 50));
-  mySprite.setPosition(50, 100);
-   Clock myclock;
+
+  Player myPlayer(myTexture);  
+  Clock myclock;
 
 
   while (window.isOpen()) {
@@ -34,22 +79,23 @@ int main() {
         window.close();
       }
       if (Keyboard::isKeyPressed(Keyboard::Left)) {
-          mySprite.move(-10 * time, 0);
-         currentFrame += 1 * time;
-          if(currentFrame >6)currentFrame -=6;
-          // INVERTING POSITION 
-          mySprite.setTextureRect(IntRect(40 * int(currentFrame)+40, 244,-40,50));
+         myPlayer.dx = -40;
 
       }
       if (Keyboard::isKeyPressed(Keyboard::Right)) {
-          mySprite.move(10* time, 0);
-          currentFrame += 1 * time;
-          if(currentFrame >6)currentFrame -=6;
-          mySprite.setTextureRect(IntRect(40 * int(currentFrame), 244,40,50));
+          myPlayer.dx = 40;
+
+      }
+      if (Keyboard::isKeyPressed(Keyboard::Up)) {
+          if(myPlayer.onGround){
+            myPlayer.dy=-0.4; myPlayer.onGround = false;
+          }
+          
       }
     }
+    myPlayer.update(time);
     window.clear();
-    window.draw(mySprite);
+    window.draw(myPlayer.sprite);
     window.display();
   }
 
