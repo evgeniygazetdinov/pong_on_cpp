67ad22c01d24db1567877a38e7ee93a8a23fa95c
Gazetdinov <yngazetd@mts.ru>
2023-05-14 06:38:08 +0300

    TODO finish game.cpp

--- game.h
+++ game.h
@@ -10,7 +10,7 @@ const int CELL_SIZE = 120;
 enum class Direction { Left=0,Right=1,Up=2, Down=3};
 
 
-class Game : public sf::Drawable, public: sf::Transformable
+class Game : public sf::Drawable, public sf::Transformable
 {
 
     protected:
